db2514f4bd5394cc3660b6c397890be0ff26788b
mr3911 <1220420614@qq.com>
2019-12-11 15:12:28 +0800

    '开发dev20191211第一次完成'

--- micro-user/src/main/java/com/shsxt/user/test.java
+++ micro-user/src/main/java/com/shsxt/user/test.java
@@ -6,6 +6,6 @@ package com.shsxt.user;
  */
 public class test {
     public static void main(String[] args) {
-        System.out.println("haha");
+        System.out.println("hahahehe");
     }
 }
