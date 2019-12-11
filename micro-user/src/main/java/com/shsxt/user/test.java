341d90368a397a8bedfa48871b0c624ad96960db
mr3911 <1220420614@qq.com>
2019-12-11 15:10:04 +0800

    '添加测试haha'

---
+++ micro-user/src/main/java/com/shsxt/user/test.java
@@ -0,0 +1,11 @@
+package com.shsxt.user;
+
+/**
+ * @author
+ * @create2019-12-11 15:08
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("haha");
+    }
+}
